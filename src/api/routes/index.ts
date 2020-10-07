a104e116067945ec039b26d825fdbbff4d164eb9
Izuchukwu Kevin <kevinfizu@gmail.com>
2020-10-07 17:06:43 +0100

    Deleting server files

--- src/api/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express'
-import { SigninController, SignoutController } from '../controllers/auth'
-
-const router = Router()
-
-router.post('/auth/signin', SigninController)
-router.post('/auth/signout', SignoutController)
-
-export { router }
--- src/utils/environment.ts
+++
@@ -1,9 +0,0 @@
-import * as dotenv from 'dotenv'
-
-const environment = process.env.NODE_ENV === 'production' ? process.env : dotenv.config().parsed!
-
-export const isDev = () => environment.NODE_ENV === 'development'
-export const isProd = () => environment.NODE_ENV === 'production'
-
-export const host = () => environment.HOST ?? ''
-export const port = () => parseInt(environment.PORT ?? '8080')

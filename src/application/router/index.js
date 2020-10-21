83e596a9855cef3272c69c9416d78381f163721c
Izuchukwu Kevin <kevinfizu@gmail.com>
2020-10-21 04:00:14 +0100

    fix(routing): using root dir as root path even if subdomain is false

--- src/application/router/index.js
+++ src/application/router/index.js
@@ -5,34 +5,46 @@ import { useSubdomain } from '@utils/enviroment'
 const subs = config.subDomains.paths || []
 const root = config.subDomains.root || 'root'
 
-const subdomains = [...subs, root]
+const subdomains = [root, ...subs]
 
-export function createRouter (ssrContext, createDefaultRouter, routerOptions) {
+const isUnderDirectory = (route, directory) => {
+	const path = route.path
+	const isUnder = (path, dir) => (path === `/${dir}`) || (path.startsWith(`/${dir}/`))
+	if (typeof directory === 'object') { return directory.some((dir) => isUnder(path, dir)) } else { return isUnder(path, directory) }
+}
+
+export const createRouter = (ssrContext, createDefaultRouter, routerOptions) => {
 	const options = routerOptions || createDefaultRouter(ssrContext).options
 
-	if (!useSubdomain) return new Router(options)
+	if (!useSubdomain) {
+		const routes = options.routes
+		const nonRootRoutes = routes.filter((route) => !isUnderDirectory(route, root))
+
+		const rootRoutes = routes
+			.filter((route) => isUnderDirectory(route, root))
+			.map((route) => ({
+				...route,
+				path: route.path.substr(root.length + 1) || '/',
+				name: route.name.substr(root.length + 1) || 'index'
+			}))
+
+		return new Router({
+			...options,
+			routes: [...nonRootRoutes, ...rootRoutes]
+		})
+	}
 
 	let routesDirectory
 
 	if (process.server && ssrContext && ssrContext.nuxt && ssrContext.req) {
 		const req = ssrContext.req
-		const matcher = req.headers.host.match(/^(\w+(-\w+)?)\.(localhost|\w+(-\w+)?)(\.\w+)?/) || ['root-domain']
+		const matcher = req.headers.host.match(/^(\w+(-\w+)?)\.(localhost|\w+(-\w+)?)(\.\w+)?/) || root
 		routesDirectory = matcher[1] || matcher[0]
 
-		routesDirectory = subdomains.includes(routesDirectory) ? routesDirectory : subdomains[subdomains.length - 1]
+		routesDirectory = subdomains.includes(routesDirectory) ? routesDirectory : subdomains[0]
 		ssrContext.nuxt.routesDirectory = routesDirectory
 	}
-	if (process.client) {
-		if (window.__NUXT__ && window.__NUXT__.routesDirectory) {
-			routesDirectory = window.__NUXT__.routesDirectory
-		}
-	}
-
-	function isUnderDirectory (route, directory) {
-		const path = route.path
-		const isUnder = (path, dir) => (path === `/${dir}`) || (path.startsWith(`/${dir}/`))
-		if (typeof directory === 'object') { return directory.some((dir) => isUnder(path, dir)) } else { return isUnder(path, directory) }
-	}
+	if (process.client && window.__NUXT__) routesDirectory = window.__NUXT__.routesDirectory
 
 	let newRoutes = options.routes
 	if (routesDirectory) {

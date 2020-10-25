0b9edd4622d185444a58d2e436e41f332e0cbe69
Izuchukwu Kevin <kevinfizu@gmail.com>
2020-10-25 09:40:29 +0100

    fix: delete redirect cookie after redirect

--- src/application/usecases/auth/signin.ts
+++ src/application/usecases/auth/signin.ts
@@ -7,15 +7,19 @@ import {
 	RegisterWithEmail,
 	SessionSignin
 } from '@modules/auth'
-import { hostname, protocol, isClient } from '@utils/environment'
+import { hostname, isClient, protocol, host } from '@utils/environment'
 import { REDIRECT_SESSION_NAME } from '@utils/constants'
 import Cookie from 'cookie'
 
 const createSession = async (id: string, idToken: string) => {
 	await SessionSignin.call(id, idToken)
-	const cookie = isClient() ? document?.cookie ?? '' : ''
-	const { [REDIRECT_SESSION_NAME]: redirect } = Cookie.parse(cookie) ?? {}
-	window.location.assign(redirect ?? protocol + hostname)
+	if (isClient()) {
+		const { [REDIRECT_SESSION_NAME]: redirect } = Cookie.parse(document?.cookie ?? '') ?? {}
+		document.cookie = Cookie.serialize(REDIRECT_SESSION_NAME, '', {
+			domain: host, expires: new Date(0)
+		})
+		window.location.assign(redirect ?? protocol + hostname)
+	} else window.location.assign(protocol + hostname)
 }
 
 export const useGoogleLogin = () => {

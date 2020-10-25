04029fe923038eb8c0935ed9ac9cfe47f3f7061e
Izuchukwu Kevin <kevinfizu@gmail.com>
2020-10-25 08:11:04 +0100

    chore: ensuring both session and id are set before authenticating user

--- src/application/store/index.ts
+++ src/application/store/index.ts
@@ -1,13 +1,13 @@
 import { Context } from '@nuxt/types'
 import Cookie from 'cookie'
-import { USERID_SESSION_NAME } from '@utils/constants'
+import { USERID_SESSION_NAME, TOKEN_SESSION_NAME } from '@utils/constants'
 import { isServer } from '@utils/environment'
 import { Action } from 'vuex'
 
 export const actions = {
 	nuxtServerInit: async (ctx, { req }: Context) => {
 		const cookies = isServer() ? Cookie.parse(req.headers.cookie ?? '') : {}
-		const id = cookies[USERID_SESSION_NAME]
-		if (id) await ctx.dispatch('auth/setId', id)
+		const { [USERID_SESSION_NAME]: id, [TOKEN_SESSION_NAME]: session } = cookies
+		if (session && id) await ctx.dispatch('auth/setId', id)
 	}
 } as { [key: string]: Action<any, any> }

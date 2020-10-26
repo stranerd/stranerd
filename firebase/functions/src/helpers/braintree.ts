846591c2c4399b08ddc36f27a1184490b5ddcb88
Izuchukwu Kevin <kevinfizu@gmail.com>
2020-10-26 01:24:41 +0100

    chore: setting up helpers in firebase functions and installing dependencies

---
+++ firebase/functions/src/helpers/braintree.ts
@@ -0,0 +1,36 @@
+import { connect, Environment, SubscriptionNotification } from 'braintree'
+import { isProduction, environmentVariables } from './environment'
+
+const getGateway = () => connect({
+	environment: Environment[isProduction() ? 'Production' : 'Sandbox'],
+	merchantId: environmentVariables.braintree.merchantId,
+	publicKey: environmentVariables.braintree.publicKey,
+	privateKey: environmentVariables.braintree.privateKey
+})
+
+export const createCustomer = async (name: string, email: string) => await getGateway().customer
+	.create({ firstName: name, email })
+
+export const getClientToken = async () => await getGateway().clientToken.generate({})
+
+export const createPaymentMethod = async (customerId: string, paymentMethodNonce: string) => await getGateway().paymentMethod
+	.create({ customerId, paymentMethodNonce })
+
+export const removePaymentMethod = async (token: string) => await getGateway().paymentMethod.delete(token)
+
+export const makePayment = async (amount: number, paymentMethodToken: string) => getGateway().transaction
+	.sale({ amount: amount.toString(), paymentMethodToken })
+
+export const subscribeToPlan = async (planId: string, paymentMethodToken: string) => await getGateway().subscription.create({
+	planId, paymentMethodToken
+})
+
+export const updatePaymentMethodForSubscription = async (id: string, paymentMethodToken: string) => await getGateway().subscription
+	// @ts-ignore TODO: FIx PlanID Issue
+	.update(id, { paymentMethodToken })
+
+export const cancelSubscription = async (id: string) => await getGateway().subscription.cancel(id)
+
+type Notification = { bt_signature: string, bt_payload: string }
+export const parseNotification = async ({ bt_signature, bt_payload }: Notification) => await getGateway()
+	.webhookNotification.parse(bt_signature, bt_payload) as SubscriptionNotification
---
+++ firebase/functions/src/helpers/mailingList.ts
@@ -0,0 +1,17 @@
+import axios from 'axios'
+import { environmentVariables } from './environment'
+
+export const mailingList = async (email: string) => {
+	const body = {
+		members: [
+			{ email_address: email, status: 'subscribed' }
+		]
+	}
+	const bodyJSON = JSON.stringify(body)
+	const { audienceId, apiKey, dataCenter } = environmentVariables.mailchimp
+	const url = `https://${dataCenter}.api.mailchimp.com/3.0/lists/${audienceId}`
+
+	try{
+		await axios.post(url, bodyJSON, { headers: { Authorization: `auth ${apiKey}` } })
+	} catch (error) { throw new Error(error.message) }
+}

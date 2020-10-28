c6c134b903b24e6c0c5f7b4e7b8e9422fc7fc0b9
Izuchukwu Kevin <kevinfizu@gmail.com>
2020-10-28 23:10:43 +0100

    fix: cleaning up functions environment file

--- firebase/functions/src/helpers/environment.ts
+++ firebase/functions/src/helpers/environment.ts
@@ -8,34 +8,33 @@ const environment = () => isProduction() ? 'production' : 'development'
 export const environmentVariables = {
 	appName: 'Stranerd',
 	paypal: {
-		clientId: functions.config().paypal[environment()]['client_id'],
-		clientSecret: functions.config().paypal[environment()]['client_secret'],
+		clientId: functions.config().paypal?.[environment()]?.['client_id'],
+		clientSecret: functions.config().paypal?.[environment()]?.['client_secret'],
 	},
 	braintree: {
-		merchantId: functions.config().braintree[environment()]['merchant_id'],
-		publicKey: functions.config().braintree[environment()]['public_key'],
-		privateKey: functions.config().braintree[environment()]['private_key']
+		merchantId: functions.config().braintree?.[environment()]?.['merchant_id'],
+		publicKey: functions.config().braintree?.[environment()]?.['public_key'],
+		privateKey: functions.config().braintree?.[environment()]?.['private_key']
 	},
 	algolia: {
-		appId: functions.config().algolia[environment()]['app_id'],
-		apiKey: functions.config().algolia[environment()]['api_key'],
+		appId: functions.config().algolia?.[environment()]?.['app_id'],
+		apiKey: functions.config().algolia?.[environment()]?.['api_key'],
 	},
 	mailchimp: {
-		apiKey: functions.config().mailchimp['api_key'],
-		audienceId: functions.config().mailchimp['audience_id'],
-		dataCenter: functions.config().mailchimp['data_center']
+		apiKey: functions.config().mailchimp?.['api_key'],
+		audienceId: functions.config().mailchimp?.['audience_id'],
+		dataCenter: functions.config().mailchimp?.['data_center']
 	},
 	admin: {
 		meta: {
-			domain: functions.config().admin[environment()].meta['domain'],
-			// TODO: Add stranerd logo to nuxt static app and update this address
-			logo: functions.config().admin[environment()].meta['domain'] + '/img/stranerd_logo.png'
+			domain: functions.config().admin?.[environment()]?.meta?.['domain'],
+			logo: functions.config().admin?.[environment()]?.meta?.['domain'] + '/images/stranerd_logo.png'
 		},
 		email: {
-			email: functions.config().admin[environment()].email['email'],
-			clientId: functions.config().admin[environment()].email['client_id'],
-			clientSecret: functions.config().admin[environment()].email['client_secret'],
-			refreshToken: functions.config().admin[environment()].email['refresh_token']
+			email: functions.config().admin?.[environment()]?.email?.['email'],
+			clientId: functions.config().admin?.[environment()]?.email?.['client_id'],
+			clientSecret: functions.config().admin?.[environment()]?.email?.['client_secret'],
+			refreshToken: functions.config().admin?.[environment()]?.email?.['refresh_token']
 		}
 	}
 }

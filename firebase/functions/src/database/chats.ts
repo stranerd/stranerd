49a0896309edd8bb96b5206dc9e4542ade0373be
Izuchukwu Kevin <kevinfizu@gmail.com>
2020-11-16 06:15:44 +0100

    feat: adding chat rules and onDelete function trigger

---
+++ firebase/functions/src/database/chats.ts
@@ -0,0 +1,7 @@
+import * as functions from 'firebase-functions'
+import { deleteFromStorage } from '../helpers/storage'
+
+export const sessionChatMediaDeleted = functions.database.ref('sessions/{sessionId}/chats/{chatId}/media')
+	.onDelete(async (snap) => {
+		await deleteFromStorage(snap.val()?.path)
+	})

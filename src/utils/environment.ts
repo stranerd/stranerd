bd3ab1d7c3437da46f9344ff0a6a361f2bc37e3a
Izuchukwu Kevin <kevinfizu@gmail.com>
2020-11-19 00:10:38 +0100

    chore: moving nuxt app over to new json format

--- src/utils/environment.ts
+++ src/utils/environment.ts
@@ -1,9 +1,9 @@
-export const isDev = process.env.NODE_ENV === 'development'
-export const isProd = process.env.NODE_ENV === 'production'
+export const isDev = process.env.ENVIRONMENT === 'development'
+export const isProd = process.env.ENVIRONMENT === 'production'
 
 export const useSubdomain = JSON.parse(process.env.USE_SUBDOMAIN ?? 'false') as boolean
 
-export const port = parseInt(process.env.PORT ?? '8080')
+export const port = JSON.parse(process.env.PORT ?? '8080') as number
 export const host = process.env.HOST!
 export const hostname = isDev ? `${host}:${port}` : host
 export const ssl = JSON.parse(process.env.SSL ?? 'false') as boolean

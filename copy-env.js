e218a40cda6ef68948112eee122425529f54121c
Izuchukwu Kevin <kevinfizu@gmail.com>
2020-11-18 23:03:26 +0100

    chore: added script to copy from env.json to .env

---
+++ copy-env.js
@@ -0,0 +1,15 @@
+const fs = require('fs')
+
+if (fs.existsSync('env.json')) {
+	const content = fs.readFileSync('env.json').toString()
+	const envs = JSON.parse(content)
+	const entries = Object.entries(envs).map(([key, value]) => ([key, JSON.stringify(value)]))
+	const envFormattedEntries = entries.reduce((accumulator, currentValue) => {
+		const [key, value] = currentValue
+		return accumulator + `${key}=${value}\n`
+	}, '')
+	fs.writeFileSync('.env', envFormattedEntries)
+} else {
+	console.log('Env.json doesnt exist. Try creating one by running npm env:copy:example')
+	process.exit(1)
+}

701c8d16b0bcb7bd93a8620ae7f12db63b88ae7f
Izuchukwu Kevin <kevinfizu@gmail.com>
2020-12-02 18:22:59 +0100

    fix: completed ui for update profile

--- src/application/components/core/modals/edit/EditAccountProfile.vue
+++ src/application/components/core/modals/edit/EditAccountProfile.vue
@@ -7,21 +7,62 @@
 					Edit Profile
 				</h4>
 				<a @click.prevent="closeEditModal">
-					<i class="fas fa-times text-danger" />
+					<i class="fas fa-times text-danger fa-lg" />
 				</a>
 			</div>
 		</template>
+		<form @submit.prevent="updateProfile">
+			<div class="form-group my-3 d-flex flex-column align-items-center">
+				<img v-if="imageLink" :src="imageLink" alt="Profile Image" class="profile-image mb-1" style="width: 150px; height:150px;">
+				<input ref="image" type="file" class="d-none" accept="image/*" @change="catchImage">
+				<a @click.prevent="() => { $refs.image.value= ''; $refs.image.click() }">
+					<span class="text-info">Change Profile Image</span>
+				</a>
+				<small v-if="factory.errors.image" class="small text-danger d-block">{{ factory.errors.image }}</small>
+			</div>
+			<div class="form-group my-3">
+				<label class="label">Your Name</label>
+				<input
+					v-model="factory.name"
+					class="form-control"
+					placeholder="Eg. John Doe"
+					:class="{'is-invalid': factory.errors.name, 'is-valid': factory.isValid('name')}"
+				>
+				<small v-if="factory.errors.name" class="small text-danger d-block">{{ factory.errors.name }}</small>
+			</div>
+			<div class="d-flex justify-content-end my-3">
+				<button class="btn btn-gold" type="submit" :disabled="loading || !factory.valid">
+					Save Profile
+				</button>
+			</div>
+			<PageLoading v-if="loading" class="mr-2" />
+			<DisplayError :error="error" />
+			<DisplaySuccess :message="message" />
+		</form>
 	</Modal>
 </template>
 
 <script lang="ts">
-import { defineComponent } from '@nuxtjs/composition-api'
+import { defineComponent, ref } from '@nuxtjs/composition-api'
 import { useEditModal } from '@app/hooks/core/modals'
+import { useUpdateProfile } from '@app/hooks/users/account'
+import { useFileInputs } from '@app/hooks/core/forms'
+import { isClient } from '@utils/environment'
 export default defineComponent({
 	name: 'EditAccountProfile',
 	setup () {
+		const { factory, error, loading, message, updateProfile } = useUpdateProfile()
 		const { closeEditModal } = useEditModal()
+		const imageLink = ref((factory.value.image as any)?.link)
+		const { catchFiles: catchImage } = useFileInputs(
+			(file:File) => {
+				factory.value.image = file
+				if (isClient()) imageLink.value = window.URL.createObjectURL(file)
+			}
+		)
 		return {
+			factory, error, loading, message, updateProfile,
+			catchImage, imageLink,
 			closeEditModal
 		}
 	}

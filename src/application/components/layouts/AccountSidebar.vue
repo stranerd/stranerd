bc9e15cc900779f8504dbeceb1dac45acb8ea4af
Izuchukwu Kevin <kevinfizu@gmail.com>
2020-12-12 20:39:19 +0100

    fix: added account sidebar icons

--- src/application/components/layouts/AccountSidebar.vue
+++ src/application/components/layouts/AccountSidebar.vue
@@ -6,28 +6,66 @@
 		<div v-if="isLoggedIn" class="d-flex flex-column align-items-center my-1">
 			<img :src="user.image" alt="" class="profile-image" style="width:90px;height:90px;">
 			<span class="text-18">{{ user.name }}</span>
+			<div class="d-flex justify-content-around my-2 w-100">
+				<div class="d-flex flex-column align-items-center">
+					<span class="small">
+						Credits
+					</span>
+					<span class="font-weight-bold">
+						{{ user.account.credits }}
+					</span>
+				</div>
+				<div class="d-flex flex-column align-items-center">
+					<span class="small">
+						Friends
+					</span>
+					<span class="font-weight-bold">
+						0
+					</span>
+				</div>
+				<div class="d-flex flex-column align-items-center">
+					<span class="small">
+						Questions
+					</span>
+					<span class="font-weight-bold">
+						{{ user.meta.questionCount }}
+					</span>
+				</div>
+				<div class="d-flex flex-column align-items-center">
+					<span class="small">
+						Answers
+					</span>
+					<span class="font-weight-bold">
+						{{ user.meta.answerCount }}
+					</span>
+				</div>
+			</div>
 		</div>
 		<div class="my-1 d-flex flex-column links">
 			<BaseLink class="link" to="/account/questions" :root="true">
-				<img src="@/assets/images/icons/dashboard.svg" alt="" width="24" height="24">
+				<img src="@/assets/images/icons/questions.svg" alt="" width="24" height="24">
 				<span class="ml-1 text-18">Questions</span>
 			</BaseLink>
 			<BaseLink class="link" to="/account/answers" :root="true">
-				<img src="@/assets/images/icons/tutors.svg" alt="" width="24" height="24">
+				<img src="@/assets/images/icons/answers.svg" alt="" width="24" height="24">
 				<span class="ml-1 text-18">Answers</span>
 			</BaseLink>
 			<BaseLink class="link" to="/account/friends" :root="true">
-				<img src="@/assets/images/icons/flashcards.svg" alt="" width="24" height="24">
+				<img src="@/assets/images/icons/friends.svg" alt="" width="24" height="24">
 				<span class="ml-1 text-18">Friends</span>
 			</BaseLink>
 			<BaseLink class="link" to="/account/projects" :root="true">
-				<img src="@/assets/images/icons/testprep.svg" alt="" width="24" height="24">
+				<img src="@/assets/images/icons/projects.svg" alt="" width="24" height="24">
 				<span class="ml-1 text-18">Projects</span>
 			</BaseLink>
 			<BaseLink class="link" to="/account/e-wallet" :root="true">
-				<img src="@/assets/images/icons/shop.svg" alt="" width="24" height="24">
+				<img src="@/assets/images/icons/e-wallet.svg" alt="" width="24" height="24">
 				<span class="ml-1 text-18">E-Wallet</span>
 			</BaseLink>
+			<a class="link" @click.prevent="openEditProfileModal">
+				<img src="@/assets/images/icons/edit-profile.svg" alt="" width="24" height="24">
+				<span class="ml-1 text-18">Edit Profile</span>
+			</a>
 		</div>
 	</div>
 </template>
@@ -35,11 +73,16 @@
 <script lang="ts">
 import { defineComponent } from '@nuxtjs/composition-api'
 import { useAuth } from '@app/hooks/auth/auth'
+import { useEditModal, useMenuModal } from '@app/hooks/core/modals'
 export default defineComponent({
 	name: 'AccountSidebar',
 	setup () {
 		const { isLoggedIn, user } = useAuth()
-		return { isLoggedIn, user }
+		const openEditProfileModal = () => {
+			useMenuModal().closeMenuModal()
+			useEditModal().setEditModalAccountProfile()
+		}
+		return { isLoggedIn, user, openEditProfileModal }
 	}
 })
 </script>

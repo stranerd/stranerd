b9ae0d0af769c923f37c3f950fb571e846f13709
Izuchukwu Kevin <kevinfizu@gmail.com>
2020-12-21 20:03:38 +0100

    fix: side menu scroll issue

---
+++ src/application/components/modals/SideMenu.vue
@@ -0,0 +1,51 @@
+<template>
+	<div class="full">
+		<div class="sidebar">
+			<div class="py-1 px-2">
+				<a @click.prevent="close">
+					<img src="@/assets/images/icons/close.svg" alt="" width="18" height="18">
+				</a>
+			</div>
+			<hr class="thin">
+			<div class="px-1">
+				<slot />
+			</div>
+		</div>
+	</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+export default defineComponent({
+	name: 'SideMenu',
+	props: {
+		close: {
+			type: Function,
+			required: true
+		}
+	}
+})
+</script>
+
+<style lang="scss" scoped>
+.full {
+	position: fixed;
+	top: 0;
+	left: 0;
+	width: 100vw;
+	height: 100vh;
+	z-index: 1;
+	background: $color-black-transparent;
+	overflow-y: auto;
+	.sidebar {
+		width: clamp(200px, 80%, 300px);
+		min-height: 100vh;
+		background: $color-white;
+		animation: slide-right .5s;
+	}
+}
+@keyframes slide-right {
+	from { left: -100px; }
+	to { left: 0; }
+}
+</style>
--- src/application/components/modals/menus/AccountSidebarMenu.vue
+++ src/application/components/modals/menus/AccountSidebarMenu.vue
@@ -1,17 +1,7 @@
 <template>
-	<div class="full">
-		<div class="sidebar">
-			<div class="py-1 px-2">
-				<a @click.prevent="closeMenuModal">
-					<img src="@/assets/images/icons/close.svg" alt="" width="18" height="18">
-				</a>
-			</div>
-			<hr class="thin">
-			<div class="px-1">
-				<AccountSidebar />
-			</div>
-		</div>
-	</div>
+	<SideMenu :close="closeMenuModal">
+		<AccountSidebar />
+	</SideMenu>
 </template>
 
 <script lang="ts">
@@ -20,35 +10,10 @@ import AccountSidebar from '@app/components/layouts/sidebars/AccountSidebar.vue'
 import { useMenuModal } from '@app/hooks/core/modals'
 export default defineComponent({
 	name: 'AccountSidebarMenu',
-	components: {
-		AccountSidebar
-	},
+	components: { AccountSidebar },
 	setup () {
 		const { closeMenuModal } = useMenuModal()
 		return { closeMenuModal }
 	}
 })
 </script>
-
-<style lang="scss" scoped>
-.full {
-	position: fixed;
-	top: 0;
-	left: 0;
-	z-index: 1;
-	width: 100vw;
-	min-height: 100vh;
-	background: $color-black-transparent;
-	.sidebar {
-		width: clamp(200px, 80%, 300px);
-		position: relative;
-		background: $color-white;
-		min-height: 100vh;
-		animation: slide-right .5s;
-	}
-}
-@keyframes slide-right {
-	from { left: -100px; }
-	to { left: 0; }
-}
-</style>

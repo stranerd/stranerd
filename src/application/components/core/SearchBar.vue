e352fed7f84e1d7f852fc912174fb959692f2fd0
Kevin Izuchukwu <kevinfizu@gmail.com>
2021-05-10 21:43:00 +0100

    fix: fixing up search bar

--- src/application/components/core/SearchBar.vue
+++ src/application/components/core/SearchBar.vue
@@ -1,8 +1,8 @@
 <template>
 	<form>
-		<input class="form-control" placeholder="Search for anything">
+		<input class="form-control" placeholder="Search for">
 		<select class="form-select form-select-sm" type="search">
-			<option v-for="key in SEARCH_TYPES" :key="key" :value="key">
+			<option v-for="key in searchTerms" :key="key" :value="key">
 				{{ key }}
 			</option>
 		</select>
@@ -12,35 +12,47 @@
 <script lang="ts">
 import { defineComponent, ref } from '@nuxtjs/composition-api'
 enum SEARCH_TYPES {
-	Questions,
-	Users
+	Questions = 'Questions',
+	Users = 'Users'
 }
 export default defineComponent({
 	name: 'SearchBar',
 	setup () {
 		const search = ref('')
-		return { search, SEARCH_TYPES }
+		const searchTerms = Object.keys(SEARCH_TYPES)
+		return { search, SEARCH_TYPES, searchTerms }
 	}
 })
 </script>
 
 <style lang="scss" scoped>
 form {
+	flex-grow: 1;
 	display: flex;
 	align-items: center;
 	font-size: 0.9rem;
 	input {
-		color: $color-white !important;
-		border: none !important;
-		box-shadow: none !important;
+		color: inherit;
+		border: none;
+		outline: none;
+		box-shadow: none;
 		min-height: unset;
 		padding: 0 0.5rem;
 	}
+	input:focus {
+		color:inherit;
+		box-shadow: none;
+	}
 	select {
 		width: auto;
-		color: $color-white !important;
 		background: inherit;
 		border: none;
+		padding: 0.25rem;
+		color: inherit;
+	}
+	select:focus option {
+		background: $color-blue;
+		color: $color-white;
 	}
 }
 </style>

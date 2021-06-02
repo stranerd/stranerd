984d432ee181a44713a5b9d89bbcd41f580f3a64
Izuchukwu Kevin <kevinfizu@gmail.com>
2021-06-02 20:08:45 +0100

    fix: fix

--- src/application/components/users/notifications/LatestNotificationsList.vue
+++ src/application/components/users/notifications/LatestNotificationsList.vue
@@ -24,14 +24,8 @@ import NotificationsListCard from '@app/components/users/notifications/Notificat
 export default defineComponent({
 	name: 'LatestNotificationsList',
 	components: { NotificationsListCard },
-	props: {
-		userId: {
-			type: String,
-			required: true
-		}
-	},
-	setup (props) {
-		const { notifications, error, loading, listener, hasMore, fetchOlderNotifications } = useNotificationList(props.userId)
+	setup () {
+		const { notifications, error, loading, listener, hasMore, fetchOlderNotifications } = useNotificationList()
 		onMounted(() => {
 			if (!listener.value) listener.startListener()
 		})

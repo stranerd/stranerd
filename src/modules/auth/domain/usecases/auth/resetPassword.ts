d3c7a80ade52345bf56a6640c86e24cad045f353
Izuchukwu Kevin <kevinfizu@gmail.com>
2021-06-12 19:55:27 +0100

    feat: auth module

---
+++ src/modules/auth/domain/usecases/auth/resetPassword.ts
@@ -0,0 +1,15 @@
+import { IAuthRepository } from '../../irepositories/iauth'
+import { PasswordResetFactory } from '../../factories/passwordReset'
+
+export class ResetPasswordUseCase {
+	private repository: IAuthRepository
+
+	constructor (repository: IAuthRepository) {
+		this.repository = repository
+	}
+
+	async call (factory: PasswordResetFactory) {
+		const { email } = await factory.toModel()
+		return await this.repository.resetPassword(email)
+	}
+}

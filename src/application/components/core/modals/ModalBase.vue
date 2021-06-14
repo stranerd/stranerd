cc79e36fe69e4b357bb5e4c648a249662edbd4d5
Izuchukwu Kevin <kevinfizu@gmail.com>
2021-06-14 20:35:26 +0100

    feat: modal stack

--- src/application/components/core/modals/ModalBase.vue
+++ src/application/components/core/modals/ModalBase.vue
@@ -6,6 +6,9 @@
 		<AccountModal />
 		<SessionModal />
 		<PaymentModal />
+		<h1>HELP</h1>
+		<h1>{{ stack }}</h1>
+		<component :is="ModalTypes[modal]" v-for="modal in []" :key="modal" :close="removeFromStack(modal)" />
 	</div>
 </template>
 
@@ -17,6 +20,7 @@ import MenuModal from '@app/components/modals/menus/MenuIndex.vue'
 import AccountModal from '@app/components/modals/account/AccountIndex.vue'
 import SessionModal from '@app/components/modals/sessions/SessionIndex.vue'
 import PaymentModal from '@app/components/modals/payments/PaymentIndex.vue'
+import { useModal } from '@app/hooks/core/modals'
 export default defineComponent({
 	name: 'ModalBase',
 	components: {
@@ -26,6 +30,10 @@ export default defineComponent({
 		AccountModal,
 		SessionModal,
 		PaymentModal
+	},
+	setup () {
+		const { stack, ModalTypes, removeFromStack } = useModal()
+		return { stack, removeFromStack, ModalTypes }
 	}
 })
 </script>

008789a7324535f90386c65206136edc68dc8557
Izuchukwu Kevin <kevinfizu@gmail.com>
2021-06-14 17:56:48 +0100

    fix: show notification bell if logged out

--- src/application/components/layouts/topNavigations/NotificationBell.vue
+++ src/application/components/layouts/topNavigations/NotificationBell.vue
@@ -2,7 +2,7 @@
 	<NuxtLink to="/notifications">
 		<span class="position-relative">
 			<img src="@app/assets/images/icons/notification.svg" alt="">
-			<i v-if="notifications.filter((n) => !n.seen).length > 0" class="fas fa-circle text-danger position-absolute" style="top: 0;right:0;font-size:0.6em" />
+			<i v-if="(notifications || []).filter((n) => !n.seen).length > 0" class="fas fa-circle text-danger position-absolute" style="top: 0;right:0;font-size:0.6em" />
 		</span>
 	</NuxtLink>
 </template>
@@ -15,7 +15,7 @@ export default defineComponent({
 	setup () {
 		const { notifications, listener } = useNotificationList()
 		onMounted(() => {
-			if (!listener.value) listener.startListener()
+			if (listener && !listener.value) listener.startListener()
 		})
 		return { notifications }
 	}

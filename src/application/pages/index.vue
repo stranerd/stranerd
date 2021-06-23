a36fdc1fe33ad579cac8d93fc0389716057fd3b1
Izuchukwu Kevin <kevinfizu@gmail.com>
2021-06-23 01:24:50 +0100

    fix: pr review

--- src/application/pages/index.vue
+++ src/application/pages/index.vue
@@ -17,7 +17,7 @@
 			<div class="phone-img">
 				<img src="@app/assets/images/homepage/phone.png" alt="" class="w-100">
 			</div>
-			<div class="details gap-0-5 gap-lg-1-5">
+			<div class="details gap-0-5 gap-lg-1-5 py-1">
 				<p>
 					<span class="d-block">Already Trusted By</span>
 					<span class="num d-block bold">30,000+</span>
@@ -28,48 +28,36 @@
 				</div>
 			</div>
 		</div>
-		<div class="bg-white py-3">
-			<div class=" sectionThird d-flex w-100">
-				<div class="d-flex flex-column justify-content-center align-items-center  flex-md-row">
-					<article class="col-30 col-md-100 p-1 text-center">
-						<img src="@app/assets/images/homepage/chat.svg" alt="">
-						<h5>Chat Sessions</h5>
-						<p>
-							Spend quality time on Chat with Nerds who help you study, solve problems and explain solutions thoroughly.
-						</p>
-					</article>
-					<article class="col-30 col-md-100 p-1 text-center">
-						<img src="@app/assets/images/homepage/green-tick.svg" alt="">
-						<h5>Best Answers</h5>
-						<p>
-							Your questions get multiple answers and you’re able to choose which is best suitable for you.
-						</p>
-					</article>
-					<article class="col-30 col-md-100 p-1 text-center">
-						<img src="@app/assets/images/homepage/nerd.svg" alt="">
-						<h5>Become A Nerd</h5>
-						<p>
-							Answer Questions & Host Chat Sessions to help other students in the subject you are Very Good in.
-						</p>
-					</article>
-				</div>
-			</div>
-			<div class="thick mx-5" />
-
-			<div class="sectionFourth d-flex flex-column w-100">
-				<h1 class="text-center mx-auto my-3">
-					How It Works
-				</h1>
-
-				<div class="d-flex align-items-center">
-					<div class="text">
-						<h1>Meet A Nerd</h1>
-						<p>
-							Nerds are smart students who are very good in a particular subject and are always ready to help other students
-							out with their difficulties in that subject. <br> Get access to a pool of Nerds and pick your choice, have a One on
-							One Chat Session with them and get them to become your study buddy, explain difficult problems, guide you through homework and so on.
-						</p>
-					</div>
+		<div class="sectionThird gap-2">
+			<article>
+				<img src="@app/assets/images/homepage/chat.svg" alt="">
+				<h5>Chat Sessions</h5>
+				<p>
+					Spend quality time on Chat with Nerds who help you study, solve problems and explain solutions thoroughly.
+				</p>
+			</article>
+			<article>
+				<img src="@app/assets/images/homepage/green-tick.svg" alt="">
+				<h5>Best Answers</h5>
+				<p>
+					Your questions get multiple answers and you’re able to choose which is best suitable for you.
+				</p>
+			</article>
+			<article>
+				<img src="@app/assets/images/homepage/nerd.svg" alt="">
+				<h5>Become A Nerd</h5>
+				<p>
+					Answer Questions & Host Chat Sessions to help other students in the subject you are Very Good in.
+				</p>
+			</article>
+		</div>
+		<div class="bg-blue-grey thin mx-2 mx-md-3 mx-lg-5 my-0" />
+		<div class="sectionFourth gap-3">
+			<h1 class="text-center mx-auto my-0">
+				How It Works
+			</h1>
+			<div class="d-flex gap-1 flex-lg-row-reverse">
+				<div class="images">
 					<div class="img-contain">
 						<img src="@app/assets/images/homepage/tn2.svg" alt="">
 					</div>
@@ -77,45 +65,51 @@
 						<img src="@app/assets/images/homepage/tn3.svg" alt="">
 					</div>
 				</div>
-
-				<div class="d-flex align-items-center mt-2">
+				<div class="text text-end">
+					<h1>Meet A Nerd</h1>
+					<p>
+						Nerds are smart students who are very good in a particular subject and are always ready to help other students
+						out with their difficulties in that subject. <br> Get access to a pool of Nerds and pick your choice, have a One on
+						One Chat Session with them and get them to become your study buddy, explain difficult problems, guide you through homework and so on.
+					</p>
+				</div>
+			</div>
+			<div class="d-flex gap-1 flex-lg-row">
+				<div class="images">
 					<div class="img-contain">
 						<img src="@app/assets/images/homepage/tn4.svg" alt="">
 					</div>
 					<div class="img-contain">
 						<img src="@app/assets/images/homepage/tn5.svg" alt="">
 					</div>
-
-					<div class="text">
-						<h1>Ask A Question</h1>
-						<p>
-							Post a Question, the question public and all Nerds can see it.
-							You will get answers from multiple Nerds and will be able to rate the answers,
-							choose which is best and Tip Nerds for their effort.
-						</p>
-					</div>
 				</div>
-
-				<div class="d-flex align-items-center mt-2">
-					<div class="text">
-						<h1>Coin System</h1>
-						<p>
-							Purchase affordable coins to interact on the website. Bronze coins are used to Ask A Question, for as low as 4 coins per question.
-							Gold coins are used to have One on One Chat Sessions with Nerds & used to Tip Nerds.
-						</p>
-					</div>
-
-					<div class="img-contain">
-						<img src="@app/assets/images/homepage/tn6.svg" alt="">
-					</div>
+				<div class="text">
+					<h1>Ask A Question</h1>
+					<p>
+						Post a Question, the question public and all Nerds can see it.
+						You will get answers from multiple Nerds and will be able to rate the answers,
+						choose which is best and Tip Nerds for their effort.
+					</p>
+				</div>
+			</div>
+			<div class="d-flex gap-1 flex-lg-row-reverse">
+				<div class="img-contain">
+					<img src="@app/assets/images/homepage/tn6.svg" alt="">
+				</div>
+				<div class="text text-end">
+					<h1>Coin System</h1>
+					<p>
+						Purchase affordable coins to interact on the website. Bronze coins are used to Ask A Question, for as low as 4 coins per question.
+						Gold coins are used to have One on One Chat Sessions with Nerds & used to Tip Nerds.
+					</p>
 				</div>
 			</div>
 		</div>
-		<div class="sectionFifth p-3">
-			<h4 class="text-center text-white">
+		<div class="sectionFifth">
+			<h4 class="text-center">
 				The solutions to your problems are just one Click away.
 			</h4>
-			<NuxtLink to="/auth" class="btn-secondary mx-1 font-weight-bold">
+			<NuxtLink to="/auth" class="btn-secondary font-weight-bold">
 				Sign up
 			</NuxtLink>
 		</div>
@@ -131,10 +125,7 @@ export default defineComponent({
 </script>
 
 <style scoped lang="scss">
-.mx-5{
-	margin: 0px 80px;
-}
-.hero{
+.hero {
 	min-height: calc(50vh - 60px);
 	color: #214A73;
 	display: flex;
@@ -147,7 +138,7 @@ export default defineComponent({
 	}
 	@media (min-width: $lg) {
 		padding: 6rem;
-		min-height: calc(100vh - 60px);
+		min-height: calc(90vh - 60px);
 	}
 	.text-con{
 		h3, h4 { font-weight: bold; }
@@ -165,7 +156,7 @@ export default defineComponent({
 	}
 }
 
-.sectionTwo{
+.sectionTwo {
 	display: flex;
 	align-items: center;
 	background: $color-blue;
@@ -216,86 +207,93 @@ export default defineComponent({
 		}
 	}
 }
-.sectionThird{
-	max-width: 89vw;
-	margin: 10px 60px;
-	@media (max-width: $md) {
-		max-width: 100%;
-		margin: 0;
-		padding: 2rem 3rem;
 
-	}
-article{
-	width: 30%;
-	@media (max-width: $md) {
-		width: 100%;
-	}
-	img{
-	margin-bottom: 1rem;
-}
-}
-}
-.sectionFourth{
-	max-width: 89vw;
-	margin: 10px 60px;
-		@media (max-width: $md) {
-		max-width: 100%;
-		margin: 0;
-		padding: 2rem 3rem;
-	}
-		@media (max-width: $sm) {
-			padding: 1rem;
-		}
+.thin { height: 1.5px; }
 
-	.d-flex{
-		@media (max-width: $md) {
-			flex-wrap: wrap;
+.sectionThird {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	padding: 2rem;
+	@media (min-width: $md) {
+		flex-direction: row;
+		justify-content: space-between;
+		align-items: flex-start;
+		padding: 3rem;
+		margin: 0 auto;
+	}
+	@media (min-width: $lg) { padding: 5rem; }
+	article {
+		@media (min-width: $md) { max-width: 30%; }
+		flex-grow: 1;
+		text-align: center;
+		img {
+			margin-bottom: 0.5rem;
+			width: 90px;
+			height: 90px;
 		}
-		@media (max-width: $sm) {
-			flex-wrap: nowrap;
+		h5 {
+			font-size: 1.25rem;
+			@media (min-width: $lg) { font-size: 1.875rem; }
+		}
+		p {
+			margin: 0;
+			max-width: 40ch;
+			font-size: 1rem;
+			@media (min-width: $md) { font-size: 1.125rem; }
+		}
+	}
+}
+
+.sectionFourth {
+	display: flex;
+	flex-direction: column;
+	padding: 2rem;
+	@media (min-width: $md) { padding: 3rem; }
+	@media (min-width: $lg) { padding: 5rem; }
+	& > .d-flex {
+		flex-direction: column;
+		align-items: center;
+		& > * { flex: 1; }
+		.images {
+			display: flex;
 			flex-direction: column;
-			justify-content: center;
 			align-items: center;
+			@media (min-width: $md) { flex-direction: row; }
 		}
 	}
-	h1{
+
+	h1 {
+		font-size: 2rem;
 		color: $color-tint-blue;
-		// text-align: center;
 	}
 
-	.text{
-
-		@media (max-width: $md) {
-			order: 3;
-			margin-left: 1rem;
-		}
-		width: 70%;
-			@media (max-width: $md) {
-			width: 100%;
-		}
-		h1{
-			font-size: 2rem;
-			text-align: end;
-
-		}
+	p {
+		font-size: 1.125rem;
+		margin-bottom: 0;
 	}
 }
-.sectionFifth{
+
+.sectionFifth {
 	display: flex;
 	flex-direction: column;
 	justify-content: center;
 	align-items: center;
+	background: $color-blue;
+	color: $color-white;
 	min-height: 300px;
+	padding: 1rem;
+	@media (min-width: $md) { padding: 2.5rem; }
+	@media (min-width: $lg) { padding: 5rem; }
 	h4{
+		margin: 0 0 1rem;
 		font-weight: normal !important;
-		font-size: 230%;
-		@media (max-width: $sm) {
-			font-size: 170%;
-		}
+		font-size: 1.125rem;
+		@media (min-width: $md) { font-size: 2.25rem; }
+		@media (min-width: $lg) { font-size: 3rem; }
 	}
 
 	.btn-secondary{
-		margin-top: 2rem;
 		font-size: 2rem;
 		padding: 13px 85px;
 		@media (max-width: $sm) {

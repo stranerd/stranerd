ff440b3352d87053b7ff42143a3f0fd698bd01e2
kevinand11 <kevinfizu@gmail.com>
2021-07-17 16:02:03 +0100

    feat: question and answer report module

--- src/modules/reports/index.ts
+++ src/modules/reports/index.ts
@@ -7,12 +7,34 @@ import { ReportFactory } from './domain/factories/report'
 import { ReportEntity } from './domain/entities/report'
 
 const userReportDataSource = new ReportFirebaseDataSource<'users', UserBio>('users')
+const questionReportDataSource = new ReportFirebaseDataSource<'questions', QuestionReportType>('questions')
+const answerReportDataSource = new ReportFirebaseDataSource<'answers', AnswerReportType>('answers')
 
 const userReportTransformer = new ReportTransformer<UserBio>()
+const questionReportTransformer = new ReportTransformer<QuestionReportType>()
+const answerReportTransformer = new ReportTransformer<AnswerReportType>()
 
-const userReportRepository = new ReportRepository<'users', UserBio>(userReportDataSource, userReportTransformer)
+const userReportRepository = new ReportRepository(userReportDataSource, userReportTransformer)
+const questionReportRepository = new ReportRepository(questionReportDataSource, questionReportTransformer)
+const answerReportRepository = new ReportRepository(answerReportDataSource, answerReportTransformer)
 
 export const AddUserReport = new AddReportUseCase(userReportRepository)
+export const AddQuestionReport = new AddReportUseCase(questionReportRepository)
+export const AddAnswerReport = new AddReportUseCase(answerReportRepository)
 
 export class UserReportFactory extends ReportFactory<UserBio> {}
+export class QuestionReportFactory extends ReportFactory<QuestionReportType> {}
+export class AnswerReportFactory extends ReportFactory<AnswerReportType> {}
+
 export class UserReportEntity extends ReportEntity<UserBio> {}
+export class QuestionReportEntity extends ReportEntity<QuestionReportType> {}
+export class AnswerReportEntity extends ReportEntity<AnswerReportType> {}
+
+export type QuestionReportType = {
+	body: string
+}
+export type AnswerReportType = {
+	title: string
+	body: string
+	questionId: string
+}

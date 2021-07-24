a38bc1d4be9af0566169059dfe914a30a9487468
kevinand11 <kevinfizu@gmail.com>
2021-07-24 13:53:50 +0100

    fix: new session

--- firebase/functions/src/onCall/sessions/requestNewSession.ts
+++ firebase/functions/src/onCall/sessions/requestNewSession.ts
@@ -44,7 +44,7 @@ export const requestNewSession = functions.https.onCall(async (data, context) =>
 				[`profiles/${tutorId}/account/meta/tutorSessions/${sessionId}`]: true,
 				[`profiles/${tutorId}/session/lobby/${sessionId}`]: true,
 				// Chat Data
-				[`chats/single/${getChatsPath(path)}/${chatId}`]: data,
+				[`chats/single/${getChatsPath(path)}/${chatId}`]: chat,
 				[`chats/meta/${path[0]}/${path[1]}/last`]: { ...chat, id: chatId },
 				[`chats/meta/${path[1]}/${path[0]}/last`]: { ...chat, id: chatId },
 				[`chats/meta/${path[1]}/${path[0]}/unRead/${chatId}`]: true

74d7a1ef07dfd133dc2052400006ff55d758c936
kevinand11 <kevinfizu@gmail.com>
2021-08-05 15:03:28 +0100

    fix: server error

--- src/application/hooks/users/roles/tutors.ts
+++ src/application/hooks/users/roles/tutors.ts
@@ -1,11 +1,11 @@
-import { computed, ssrRef, useFetch } from '@nuxtjs/composition-api'
+import { computed, reqRef, useFetch } from '@nuxtjs/composition-api'
 import { GetAllSessionTutors, ListenToAllSessionTutors, UserEntity } from '@modules/users'
 import { useErrorHandler, useListener, useLoadingHandler } from '@app/hooks/core/states'
 
 const global = {
-	tutors: ssrRef([] as UserEntity[]),
-	fetched: ssrRef(false),
-	subjectId: ssrRef(''),
+	tutors: reqRef([] as UserEntity[]),
+	fetched: reqRef(false),
+	subjectId: reqRef(''),
 	...useErrorHandler(),
 	...useLoadingHandler()
 }
@@ -28,10 +28,10 @@ export const useTutorsList = () => {
 				let matched = true
 				if (global.subjectId.value && !tutor.subjects.find((s) => s.id === global.subjectId.value)) matched = false
 				return matched
-			}).slice(0, 50),
+			}), // .slice(0, 50),
 		set: (tutors) => {
 			tutors.forEach((t) => {
-				const index = global.tutors.value.findIndex((x) => x.id === t.id)
+				const index = global.tutors.value.findIndex((x) => x.id === t?.id)
 				if (index === -1) global.tutors.value.push(t)
 				else global.tutors.value.splice(index, 1, t)
 			})

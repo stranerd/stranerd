0da48ad9aa83dff0ee33f957d656e82796bbdb55
kevinand11 <kevinfizu@gmail.com>
2021-08-05 14:22:22 +0100

    fix: tutor list filtering

--- src/application/hooks/users/roles/tutors.ts
+++ src/application/hooks/users/roles/tutors.ts
@@ -22,13 +22,20 @@ export const useTutorsList = () => {
 	}
 	const filteredTutors = computed({
 		get: () => global.tutors.value
-			.sort((a, b) => a.orderRating > b.orderRating ? -1 : a.orderRating === b.orderRating ? 0 : 1)
+			// TODO: Check if sorting is cause of empty flash in prod
+			//  .sort((a, b) => a.orderRating > b.orderRating ? -1 : a.orderRating === b.orderRating ? 0 : 1)
 			.filter((tutor) => {
 				let matched = true
 				if (global.subjectId.value && !tutor.subjects.find((s) => s.id === global.subjectId.value)) matched = false
 				return matched
 			}).slice(0, 50),
-		set: (tutors) => { global.tutors.value = tutors }
+		set: (tutors) => {
+			tutors.forEach((t) => {
+				const index = global.tutors.value.findIndex((x) => x.id === t.id)
+				if (index === -1) global.tutors.value.push(t)
+				else global.tutors.value.splice(index, 1, t)
+			})
+		}
 	})
 	const listener = useListener(async () => {
 		const appendTutors = (tutors: UserEntity[]) => { global.tutors.value = tutors }

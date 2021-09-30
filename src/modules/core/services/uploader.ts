254c112cf9f1ecd67bbe17bb4a101b3cee80f4a2
kevinand11 <kevinfizu@gmail.com>
2021-10-01 00:17:16 +0100

    fix: storage url

--- src/modules/core/services/uploader.ts
+++ src/modules/core/services/uploader.ts
@@ -3,21 +3,36 @@ import { Media } from '../data/models/base'
 import { HttpClient } from './http'
 
 export const UploaderService = {
-	call: async (path: string, file: File): Promise<Media> => {
-		return await uploadFile(path, file) as Media
-	}
+	single: async (path: string, file: File): Promise<Media> => await uploadFile(path, file),
+	multiple: async (path: string, files: File[]): Promise<Media[]> => await uploadFiles(path, files)
 }
 
-export const uploadFile = async (path: string, file: File) => {
+export const uploadFile = async (path: string, file: File): Promise<Media> => {
 	try {
 		const data = new FormData()
 		data.set('path', path)
 		data.set('file', file)
-		const storageClient = new HttpClient(apiBases.STORAGE)
-		const media = await storageClient.post<typeof data, Media>('/file', data)
-		if (!media.link) media.link = apiBases.STORAGE + '/' + media.path
+		const utilsClient = new HttpClient(apiBases.UTILS)
+		const media = await utilsClient.post<typeof data, Media>('/storage/file', data)
+		if (!media.link) media.link = apiBases.UTILS + '/' + media.path
 		return media
 	} catch {
-		throw new Error(`Error uploading ${file.name}`)
+		throw new Error('Error uploading file')
+	}
+}
+
+export const uploadFiles = async (path: string, files: File[]): Promise<Media[]> => {
+	try {
+		const data = new FormData()
+		data.set('path', path)
+		files.forEach((file) => data.append('file', file))
+		const utilsClient = new HttpClient(apiBases.UTILS)
+		const medias = await utilsClient.post<typeof data, Media[]>('/storage/files', data)
+		return medias.map((media) => {
+			if (!media.link) media.link = apiBases.UTILS + '/' + media.path
+			return media
+		})
+	} catch {
+		throw new Error('Error uploading files')
 	}
 }
